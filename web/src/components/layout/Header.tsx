e4e2c9231991704cfe0e90e4cbedd0fd1aef0406
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-25 15:24:18 +0400

    Cleared boilerplate

--- web/src/components/layout/Header.tsx
+++ web/src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export const Header = ({ action }: { action?: ReactNode }) => {
     <div className="h-16 border-b-1 border-solid border-border box-border sticky top-0 bg-white z-10">
       <div className="max-w-6xl m-auto h-full flex justify-between items-center sm:px-8 lt-sm:px-4">
         <div className="flex items-center font-bold cursor-pointer">
-          <span className="text-xl">Unoi</span>
+          <span className="text-xl">Carbon Credits</span>
           <span className="flex-col-center ml-3 py-.25 px-1.5 text-xs text-#666 bg-white rounded-full font-light">
             <span
               style={
@@ -18,7 +18,7 @@ export const Header = ({ action }: { action?: ReactNode }) => {
               }
               className="bg-clip-text"
             >
-              fisand
+              blockchain
             </span>
           </span>
         </div>
