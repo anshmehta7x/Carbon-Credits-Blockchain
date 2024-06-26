54780b97bbd8ff546ce5b98ff8fc3abb1d9f892d
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-26 12:43:25 +0400

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  // networks: {
-  //   xdctestnet: {
-  //     url: "https://earpc.apothem.network",
-  //     accounts: process.env.PRIVATE_KEY || 12323123,
-  //   },
-  // },
+  networks: {
+    hardhat: {},
+    xdctestnet: {
+      url: "https://earpc.apothem.network",
+      accounts: [
+        process.env.PRIVATE_KEY ||
+          1234567890123456789012345678901234567890123456789012345678901234,
+      ],
+    },
+    sepolia: {
+      url: "https://sepolia.drpc.org",
+      accounts: [
+        process.env.PRIVATE_KEY ||
+          1234567890123456789012345678901234567890123456789012345678901234,
+      ],
+    },
+  },
 };
